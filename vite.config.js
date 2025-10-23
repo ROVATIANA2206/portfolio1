bafcb6f318fdb3d20e75bcc222eb38335f202a67
ROVATIANA2206 <rovatiananiriniaina1@gmail.com>
2025-10-23 13:07:49 +0300

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio1/', // 👈 ajoute ceci
+  base: '/',
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.JPG', '**/*.JPEG']
 })
